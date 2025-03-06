7600978ae63c61dca41b67002d58fa510cec143c
nandanbk-112 <nandanbk112@gmail.com>
2025-03-06 14:11:08 +0530

    Add files via upload

---
+++ tik.py
@@ -0,0 +1,52 @@
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
+
+def check_winner(board):
+    for row in board:
+        if row[0] == row[1] == row[2] != ' ':
+            return row[0]
+    
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col] != ' ':
+            return board[0][col]
+    
+    if board[0][0] == board[1][1] == board[2][2] != ' ':
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != ' ':
+        return board[0][2]
+    
+    return None
+
+def is_full(board):
+    return all(cell != ' ' for row in board for cell in row)
+
+def tic_tac_toe():
+    board = [[' ' for _ in range(3)] for _ in range(3)]
+    player = 'X'
+    
+    while True:
+        print_board(board)
+        row, col = map(int, input(f"Player {player}, enter row and column (0-2): ").split())
+        
+        if board[row][col] == ' ':
+            board[row][col] = player
+        else:
+            print("Cell already occupied! Try again.")
+            continue
+        
+        winner = check_winner(board)
+        if winner:
+            print_board(board)
+            print(f"Player {winner} wins!")
+            break
+        
+        if is_full(board):
+            print_board(board)
+            print("It's a tie!")
+            break
+        
+        player = 'O' if player == 'X' else 'X'
+
+tic_tac_toe()
